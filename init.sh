7f8b7813849f6a1fd758003f6f81a9b1e8d87c5f
Aaron Biggs <abiggs@d-ip-129-15-84-54.cas.ou.edu>
2014-01-09 08:06:54 -0600

    Added gitfiles() function.

--- init.sh
+++ init.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
+gitfiles() {
+    echo "\033[0;34mLooking for an existing gitconfig config...\033[0m"
+    if [ -f ~/.gitconfig ] || [ -h ~/.gitconfig ]
+    then
+      echo "\033[0;33mFound ~/.gitconfig.\033[0m \033[0;32mBacking up to ~/.gitconfig.old\033[0m";
+      mv ~/.gitconfig ~/.gitconfig.old;
+    fi
+
+    echo "\033[0;34mUsing the gitconfig template file and adding it to ~/.gitconfig\033[0m"
+    cp ~/.dotfiles/templates/gitconfig.template ~/.gitconfig
+}
+
 ohmyzsh() {
     printf '\033[0;34m%s\033[0m\n' "Installing oh-my-zsh..."
     curl -L https://raw.github.com/abiggs/oh-my-zsh/master/tools/install.sh | sh
@@ -14,4 +26,5 @@ ohmyzsh() {
 # source ./links.sh
 # source ./osx.sh
 
-ohmyzsh
+gitfiles
+# ohmyzsh
