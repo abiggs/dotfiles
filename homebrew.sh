f079071aec31226b2c7daeb4ef840b421ff03964
Aaron Biggs <abiggs@ou.edu>
2014-01-09 09:11:45 -0600

    Updated output color.

--- homebrew.sh
+++ homebrew.sh
@@ -3,15 +3,15 @@
 # Check for Homebrew
 if test ! $(which brew)
 then
-echo "Installing Homebrew..."
+printf '\033[0;34m%s\033[0m\n' "Installing Homebrew..."
 curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install | ruby
 else
-  echo "Homebrew already installed. Updating Homebrew..."
+  printf '\033[0;34m%s\033[0m\n' "Homebrew already installed. Updating Homebrew..."
   # Make sure we’re using the latest Homebrew
   brew update
 fi
 
-echo "Installing Homebrew formulas..."
+printf '\033[0;34m%s\033[0m\n' "Installing Homebrew formulas..."
 
 # Upgrade any already-installed formulae
 brew upgrade
@@ -57,7 +57,7 @@ brew install php54-mcrypt
 # Dependency management for PHP.
 brew install josegonzalez/php/composer
 
-echo "Cleaning Homebrew Cellar..."
+printf '\033[0;34m%s\033[0m\n' "Cleaning Homebrew Cellar..."
 
 # Remove outdated versions from the cellar
 brew cleanup
