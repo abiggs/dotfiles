99fa76746fc897292efaf0d79a015d5bb31483e8
Aaron Biggs <abiggs@ou.edu>
2014-01-09 08:16:21 -0600

    Added gitignore template and added loop to add the file.

--- init.sh
+++ init.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env bash
 
 gitfiles() {
-    echo "\033[0;34mLooking for an existing gitconfig config...\033[0m"
-    if [ -f ~/.gitconfig ] || [ -h ~/.gitconfig ]
-    then
-      echo "\033[0;33mFound ~/.gitconfig.\033[0m \033[0;32mBacking up to ~/.gitconfig.old\033[0m";
-      mv ~/.gitconfig ~/.gitconfig.old;
-    fi
 
-    echo "\033[0;34mUsing the gitconfig template file and adding it to ~/.gitconfig\033[0m"
-    cp ~/.dotfiles/templates/gitconfig.template ~/.gitconfig
+    gitfiles=(gitconfig gitignore)
+    
+    for i in "${gitfiles[@]}"
+    do
+      echo "\033[0;34mLooking for an existing $i config...\033[0m"
+      if [ -f ~/.$i ] || [ -h ~/.$i ]
+      then
+        echo "\033[0;33mFound ~/.$i.\033[0m \033[0;32mBacking up to ~/.$i.old\033[0m";
+        mv ~/.$i ~/.$i.old;
+      fi
+
+      echo "\033[0;34mUsing the $i template file and adding it to ~/.$i\033[0m"
+      cp ~/.dotfiles/templates/$i.template ~/.$i
+    done
+
+   
 }
 
 ohmyzsh() {
@@ -18,7 +26,7 @@ ohmyzsh() {
 }
 
 # Install Homebrew.
-source ./homebrew.sh
+# source ./homebrew.sh
 # source ./ruby.sh
 # source ./node.sh
 # source ./capistrano.sh
