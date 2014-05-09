154351a41350c8a77abd0ec5c10be581252717c8
Aaron Biggs <abiggs@ou.edu>
2014-05-09 08:22:10 -0500

    Added some applications back to links.sh.

--- links.sh
+++ links.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 printf '\033[0;34m%s\033[0m\n' "Creating links..."
 
-apps=(TextExpander)
+apps=("PremiumSoft CyberTech" "Sublime Text 3" TextExpander Tower)
 
 source_dir=~/Library/Application\ Support
 target_dir=~/Dropbox/Synced\ Settings
