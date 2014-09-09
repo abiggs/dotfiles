90019292fa2ef9e2966f6065eab5d32d52d3b916
Aaron Biggs <abiggs@ou.edu>
2014-09-09 07:27:06 -0500

    Removed some Sublime Text modules.

--- node.sh
+++ node.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
-# Install Yeoman, Grunt, and Bower
+# Installs Yeoman and a few generators.
 printf '\033[0;34m%s\033[0m\n' "Installing Yeoman..."
 npm install -g yo
 npm install -g generator-webapp
 npm install -g generator-angular
 npm install -g generator-backbone
-
-# Installs jshint for Sublime Text linting.
-npm install -g jshint
-
-# Installs csslint for Sublime Text linting.
-npm install -g csslint
 
 # Installs Gulp.
 npm install -g gulp
