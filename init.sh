cc3e8c0b25bc2b19f41ca9b6000e3f9327619340
Aaron Biggs <abiggs@ou.edu>
2014-11-05 07:50:37 -0600

    Added Earth.

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-read -p "Is this \"Jupiter\" or \"Mars\"? " COMPUTER_NAME
+read -p "Is this \"Earth\" or \"Jupiter\"? or \"Mars\"? " COMPUTER_NAME
 case $COMPUTER_NAME in
+    Earth ) printf '\033[0;34m%s\033[0m\n' "Installing dotfiles on Earth...";;
     Jupiter ) printf '\033[0;34m%s\033[0m\n' "Installing dotfiles on Jupiter...";;
     Mars ) printf '\033[0;34m%s\033[0m\n' "Installing dotfiles on Mars...";;
     * ) echo "Please answer with \"Jupiter\" or \"Mars\"."; exit 0;;
