7221c1634b3a5622ce4844c89e07f1936b8d83e7
Aaron Biggs <abiggs@ou.edu>
2015-04-05 11:31:05 -0500

    Removed unused programs.

--- homebrew.sh
+++ homebrew.sh
@@ -37,14 +37,8 @@ brew install coreutils
 # Install grc
 brew install grc
 
-# Install Spark
-brew install spark
-
 # Install wget
 brew install wget
-
-# Install Tree
-brew install tree
 
 # Install node
 brew install node
@@ -65,17 +59,9 @@ brew cask install virtualbox
 # Links Brew Casks installed apps to Alfred
 brew cask alfred link
 
-# Install PHP 5.4
-brew tap homebrew/dupes
-brew tap josegonzalez/homebrew-php
-brew install php54
-
 # Install PHP code quality tools
 brew install php-code-sniffer
 brew install phpmd
-
-# Install Mcrypt for PHP 5.4. Required for Laravel.
-brew install php54-mcrypt
 
 # Dependency management for PHP.
 brew install josegonzalez/php/composer
