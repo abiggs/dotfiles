4668552805c9930d807feffa32a49b749097e2cf
Aaron Biggs <abiggs@ou.edu>
2015-05-31 10:36:18 -0500

    Remov vagrant.sh
    Not sure why this is here.

--- vagrant.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "-- Testing --"
-sed -i "s/post_max_size = .*/post_max_size = 25M/" /etc/php5/fpm/php.ini
-sed -i "s/upload_max_filesize = .*/upload_max_filesize = 25M/" /etc/php5/fpm/php.ini
-
-sudo service php5-fpm restart
