55a86f1f9f1900e1f6b3206d3be114f043ef93d5
Aaron Biggs <abiggs@ou.edu>
2015-08-03 10:29:02 -0500

    Disable a lot of stuff I don't use.

--- node.sh
+++ node.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # Installs Yeoman and a few generators.
-printf '\033[0;34m%s\033[0m\n' "Installing Yeoman..."
-npm install -g yo
-npm install -g generator-webapp
-npm install -g generator-angular
-npm install -g generator-backbone
+# printf '\033[0;34m%s\033[0m\n' "Installing Yeoman..."
+# npm install -g yo
+# npm install -g generator-webapp
+# npm install -g generator-angular
+# npm install -g generator-backbone
 
 # Installs jscs for Sublime Text linting.
-npm install -g jscs
+# npm install -g jscs
 
 # Installs jshint for Sublime Text linting.
-npm install -g jshint
+# npm install -g jshint
 
 # Installs csslint for Sublime Text linting.
-npm install -g csslint
+# npm install -g csslint
 
 # Installs Gulp.
 npm install -g gulp
