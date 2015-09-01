821e6d73636b150676f544c4489fdeeaacccfdfc
Aaron Biggs <abiggs@ou.edu>
2015-09-01 07:31:51 -0500

    Add evaluate.dev to hosts file.

--- init.sh
+++ init.sh
@@ -36,7 +36,7 @@ hosts() {
         sudo bash -c "echo 192.168.10.10    motivational.app >> /etc/hosts"
         sudo bash -c "echo 192.168.10.10    newsoonersurvey.app >> /etc/hosts"
         sudo bash -c "echo '# Docker Machine Sites' >> /etc/hosts"
-        sudo bash -c "echo 192.168.99.100   classnav.dev iadvise.dev >> /etc/hosts"
+        sudo bash -c "echo 192.168.99.100   classnav.dev evaluate.dev iadvise.dev >> /etc/hosts"
     fi
 }
 
