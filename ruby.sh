7c0f86cfd499f5424ef658c5dce29e59d327c1c3
Aaron Biggs <abiggs@ou.edu>
2015-09-04 16:22:59 -0500

    Update Ruby to 2.1.5.

--- ruby.sh
+++ ruby.sh
@@ -11,10 +11,10 @@ else
        printf '\033[0;34m%s\033[0m\n' "rbenv version already installed."
     else
         # Install specific version of Ruby
-        rbenv install 1.9.3-p448
+        rbenv install 2.1.5
 
         # Set Ruby version as global version
-        rbenv global 1.9.3-p448
+        rbenv global 2.1.5
 
         # Refresh the Ruby environment
         rbenv rehash
