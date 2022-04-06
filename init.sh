333c5b96b7c744f83907b219b249a21051e7fa17
Aaron Biggs <abiggs@ou.edu>
2022-04-05 20:51:55 -0500

    Fix Mac Studio name

--- init.sh
+++ init.sh
@@ -2,10 +2,10 @@
 
 read -p "Is this \"Aaron's Mac Studio\" or \"Aaron's iMac Pro\"? or \"Aaron's MacBook Pro\"? " COMPUTER_NAME
 case $COMPUTER_NAME in
-    Aaron\'s\ iMac ) printf '\033[0;34m%s\033[0m\n' "Installing dotfiles on Aaron's iMac...";;
+    Aaron\'s\ Mac\ Studio ) printf '\033[0;34m%s\033[0m\n' "Installing dotfiles on Aaron's iMac...";;
     Aaron\'s\ iMac\ Pro ) printf '\033[0;34m%s\033[0m\n' "Installing dotfiles on Aaron's iMac Pro...";;
     Aaron\'s\ MacBook\ Pro ) printf '\033[0;34m%s\033[0m\n' "Installing dotfiles on Aaron's MacBook Pro...";;
-    * ) echo "Please answer with \"Aaron's iMac\" or \"Aaron's iMac Pro\" or \"Aaron's MacBook Pro\"."; exit 0;;
+    * ) echo "Please answer with \"Aaron's Mac Studio\" or \"Aaron's iMac Pro\" or \"Aaron's MacBook Pro\"."; exit 0;;
 esac
 
 gitfiles() {
